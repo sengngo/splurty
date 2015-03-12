c08168bf7f9cd73bbf3fe86e6f8dfd012dbd529f
Seng Ngo <sengngo@gmail.com>
2015-03-12 03:47:30 +0000

    Add new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,22 @@
 class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
-	end
+	end #index
 
 	def new
 		@quote = Quote.new
-	end
-end
+	end #new
+
+	# sends the saying and author part to the data based to get saved
+	def create
+		Quote.create(quote_params)
+		redirect_to root_path
+	end #create
+
+	private
+	#quote_params defines the parameter of a quote
+	def quote_params
+		# permit - is a security feature - we are only permitting a saying and author in to the database
+		params.require(:quote).permit(:saying, :author)
+	end #quote_params
+end #Class QuotesController
