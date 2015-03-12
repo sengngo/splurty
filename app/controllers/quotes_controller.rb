fe02723fd84159a4023d534703e7a21e018eb33a
Seng Ngo <sengngo@gmail.com>
2015-03-12 04:28:09 +0000

    Validating User Data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,7 +9,11 @@ class QuotesController < ApplicationController
 
 	# sends the saying and author part to the data based to get saved
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not save</strong> the date you entered is invalid.'
+		end
+	
 		redirect_to root_path
 	end #create
 
