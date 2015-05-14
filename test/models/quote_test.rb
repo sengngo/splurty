f15c813ec23c26207eb28581e5661bd88d36a762
Seng Ngo <sengngo@gmail.com>
2015-05-14 03:34:27 +0000

    Lesson 3: Test - Building a model test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-     assert true
-  end
+ 	test "unique_tag" do 
+ 		quote = Quote.create(:author => 'Seng Ngo', :saying => "You only yolo once.")
+ 		expect = "SN#" + quote.id.to_s
+ 		actual = quote.unique_tag
+ 		assert_equal expect, actual
+ 	end
 end
